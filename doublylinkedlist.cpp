05f024c6762ee0300052bf348ad464f6d75c54cd
Vahe Barseghyan <vahebarseghyan9999@gmail.com>
2022-05-20 22:40:16 +0400

    several functions have been added

--- doublylinkedlist.cpp
+++ doublylinkedlist.cpp
@@ -1,48 +1,81 @@
-#include "doublylinkedlist.h"
+#include "linked_list.h"
 #include <iostream>
 
-DoublyLinkedList::DoublyLinkedList() {
+LinkedList::LinkedList() {
 	m_head = m_tail = nullptr;
 	m_list_size = 0;
 }
 
-DoublyLinkedList::DoublyLinkedList(const DoublyLinkedList& otherList) {
-	
-}
 
-DoublyLinkedList::~DoublyLinkedList() {
+LinkedList::~LinkedList() {
 	clear();
 }
 
-bool DoublyLinkedList::empty() {
-	if (m_list_size == 0) return true;
-	else return false;
+Node* LinkedList::front() {
+	return m_head;
 }
 
-int DoublyLinkedList::size() {
+Node* LinkedList::back() {
+	return m_tail;
+}
+
+bool LinkedList::empty() {
+	if (m_list_size == 0) 
+		return true;
+	else 
+		return false;
+}
+
+int LinkedList::size() {
 	return m_list_size;
 }
 
-void DoublyLinkedList::push_front(Node& node) {
-	Node* new_node = new Node(node);
-	new_node->m_data = node.m_data;
+void LinkedList::push_front(int data) {
+	Node* new_node = new Node;
+	new_node->m_data = data;
 
-	if (m_head != nullptr) {
-		m_head = new_node;
-		m_head->m_next = m_tail;
-		m_head->m_prev = nullptr;
-	}
-	else {
-		new_node->m_prev = nullptr;
-		new_node->m_next = m_head;
-		m_head->m_prev = new_node;
-	}
+	new_node->m_next = m_head;
+	new_node->m_prev = nullptr;
 
+	if (m_head != nullptr)
+		new_node->m_next->m_prev = new_node;
+	else 
+		m_tail = new_node;
+
+	m_head = new_node;
 	++m_list_size;
 }
 
-void DoublyLinkedList::pop_front() {
-	Node* temp = m_head;
+void LinkedList::push_back(int data) {
+	Node* new_node = new Node;
+	new_node->m_data = data;
+
+	new_node->m_prev = m_tail;
+	new_node->m_next = nullptr;
+
+	if (m_tail != nullptr)
+		new_node->m_prev->m_next = new_node;
+	else
+		m_head = new_node;
+
+	m_tail = new_node;
+	++m_list_size;
+}
+
+void LinkedList::resize(int new_size) {
+	if (new_size < m_list_size) {
+		while (new_size != m_list_size) {
+			pop_back();
+		}
+	}
+	else if (new_size > m_list_size) {
+		while (new_size != m_list_size)
+			push_back(0);
+	}
+}
+
+void LinkedList::pop_front() {
+	Node* temp = m_head;  //temporary node
 	if (m_head->m_next != nullptr) {
 		m_head = m_head->m_next;
 		m_head->m_prev = nullptr;
@@ -56,20 +89,58 @@ void DoublyLinkedList::pop_front() {
 	--m_list_size;
 }
 
-void DoublyLinkedList::clear() {
+void LinkedList::pop_back() {
+	Node* temp = m_tail;  //temporary node
+	if (m_head->m_next != nullptr) {
+		m_tail = m_tail->m_prev;
+		m_tail->m_next = nullptr;
+		delete temp;
+	}
+	else {
+		delete m_head;
+		m_tail = m_head = nullptr;
+	}
+
+	--m_list_size;
+}
+/*
+void LinkedList::unique() {
+	Node* current_node = m_head;
+	Node* last_node = m_head;
+	if (m_head->m_next == nullptr)
+		current_node = nullptr;
+
+	for (;;) {
+		current_node = current_node->m_next;
+		if (current_node == nullptr) break;
+
+		if (last_node->m_data == current_node->m_data) {
+			delete last_node;
+		}
+		else {
+			last_node = last_node->m_next;
+		}
+	}
+}
+*/
+void LinkedList::clear() {
 	while (m_head) {
 		Node* temp = m_head;
 		m_head = m_head->m_next;
 		delete temp;
-	}  
-	
-	/*
-	while(list_size) {
-		clear();
 	}
-	*/
+	m_list_size = 0;
 }
 
-void DoublyLinkedList::print() {
-	std::cout<<this->m_list_size;
+void LinkedList::print() {
+	if (m_head != nullptr) {
+		Node* current = m_head;
+
+		while (current != nullptr) {
+			std::cout << current->m_data << std::endl;
+			current = current->m_next;
+		}
+	}
+	else
+		std::cout << "List is empty!\n";
 }
