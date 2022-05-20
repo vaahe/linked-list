fd8097d77bb8e9bea802974739f75eab7162c673
Vahe Barseghyan <vahebarseghyan9999@gmail.com>
2022-05-20 22:46:17 +0400

    main file with examples

--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
-#include "doublylinkedlist.h"
+#include "linked_list.h"
 #include <iostream>
 
-
 int main() {
-    DoublyLinkedList list;
-    list.print();
+	LinkedList list;
+	list.push_front(1);
+	list.push_front(2);
+	list.push_front(3);
+	list.push_front(4);
+	list.push_front(5);
+	list.resize(7);
+	list.print();
 
-    return 0;
+	return 0;
 }
