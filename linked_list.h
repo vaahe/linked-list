a511c0188332423d530b192ced843953c85248f9
Vahe Barseghyan <vahebarseghyan9999@gmail.com>
2022-06-09 00:39:18 +0400

    change Iterator class

--- linked_list.h
+++ linked_list.h
@@ -7,67 +7,33 @@
 template <class T>
 class LinkedList {
 public:
-    template <typename>
     class Iterator {
     public:
-        explicit Iterator(T* ptr) : m_ptr(ptr) {}
-        //keyword 'explicit' is used to mark constructor to not implicitly convert types
-
-        T& operator*() const {
-            return *m_ptr;
-        }
-
-        T* operator->() const {
-            return m_ptr;
-        }
-
-        Iterator<T>& operator++()
-        {
-            ++m_ptr;
-            return *this;
-        }
-
-        Iterator<T>& operator++(T) {
-            Iterator<T> tmp = *this;
-            ++(*this);
-            return tmp;
-        }
-
-        Iterator<T>& operator--() {
-            --m_ptr;
-            return *this;
-        }
-
-        Iterator<T>& operator--(T) {
-            Iterator<T> tmp = *this;
-            --(*this);
-            return tmp;
-        }
-
-        bool operator==(const Iterator<T>& other) {
-            return m_ptr == other.m_ptr;
-        }
-
-        bool operator!=(const Iterator<T>& other)
-        {
-            return m_ptr != other.m_ptr;
-        }
+        explicit Iterator(T* ptr);
+        T& operator*() const;
+        T* operator->() const;
+        Iterator& operator++();
+        Iterator& operator++(int);
+        Iterator& operator--();
+        Iterator& operator--(int);
+        bool operator==(const Iterator& other);
+        bool operator!=(const Iterator& other);
     private:
         T* m_ptr;
+        friend class LinkedList<T>;
     };
 private:
-    template <typename>
     struct Node {
     public:
-        Node(T data = T(), Node<T>* next = nullptr, Node<T>* prev = nullptr) {
+        Node(T data = T(), Node* next = nullptr, Node* prev = nullptr) {
             m_data = data;
             m_next = next;
             m_prev = prev;
         }
 
         T m_data;
-        Node<T>* m_next;
-        Node<T>* m_prev;
+        Node* m_next;
+        Node* m_prev;
     };
 
 public:
@@ -78,21 +44,20 @@ public:
     ~LinkedList();
 
 public:
-    Node<T>* front();  //accesses the first element
-    Node<T>* back();  //accesses the last element
-    Iterator<T> begin();  //returns an iterator to the beginning
-    Iterator<T> end();  //returns an iterator to the end
+    Node* front();  //accesses the first element
+    Node* back();  //accesses the last element
+    Iterator begin();  //returns an iterator to the beginning
+    Iterator end();  //returns an iterator to the end
     bool empty() const;  //checks whether a linked list container is empty or not
     int size() const;  //finds the number of elements
     void clear();  //removes all elements
-    LinkedList<T>::Node<T> insert(Node<T>*, const T&);  //inserts the elements at any position
-    LinkedList<T>::Iterator<T> emplace(Iterator<T>, T&&);  //extends list by inserting new element at a given position
+    Node insert(Node*, const T&);  //inserts the elements at any position
+    Iterator emplace(Iterator, T&&);  //extends list by inserting new element at a given position
     void push_back(T);  //adds element at the end
     void push_front(T);  //adds element at the beginning
     void resize(int);  //changes size
-    void splice(Iterator<T>, LinkedList<T>&);  //transfers elements from one linked list to another
+    void splice(Iterator, LinkedList<T>&);  //transfers elements from one linked list to another
     LinkedList<T>& merge(LinkedList<T>&);  //merges two linked lists into one
-    //Iterator remove_if(Iterator, Iterator, bool);  //removes elements fulfilling condition
     void unique();  //removes duplicate values
     void pop_front();  //removes the first element
     void pop_back();
@@ -109,12 +74,13 @@ public:
     bool operator>=(LinkedList<T>&);
 
 private:
-    Node<T>* m_head;
-    Node<T>* m_tail;
+    Node* m_head;
+    Node* m_tail;
     int m_size;
 
-private:  //helper functions
+private:
     T sum();
+    void print();
 };
 
 #include "linked_list.hpp"
